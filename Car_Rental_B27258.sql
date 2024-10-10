06d5062ffe9ff6abd91c68c475144291f0f5313f
Plastimytes <magezirichardelijah@gmail.com>
2024-10-11 00:01:26 +0300

    Add files via upload

---
+++ Car_Rental_B27258.sql
@@ -0,0 +1,5 @@
+-- Active: 1727875585648@@127.0.0.1@3306@carrent
+ALTER TABLE `affiliated_companies` 
+	CHANGE ``AffiliatedCompanyID`` ``AffiliatedCompanyID`` int NOT NULL ;-- Active: 1727875585648@@127.0.0.1@3306@carrent
+ALTER TABLE `affiliated_companies` 
+	CHANGE ``AffiliatedCompanyID`` ``AffiliatedCompanyID`` int NOT NULL ;
