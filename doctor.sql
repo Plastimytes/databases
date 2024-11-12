4d02fb340f8ee25083a61cdbc11ba72e82556b53
Plastimytes <magezirichardelijah@gmail.com>
2024-11-11 22:12:41 -0800

    Add files via upload

---
+++ doctor.sql
@@ -0,0 +1,10 @@
+SHOW DATABASES;
+
+CREATE TABLE VEHICLE(
+     farmerID INT,
+     farmerName VARCHAR(100) NOT NULL,
+     address VARCHAR(40),
+     contact VARCHAR(40),
+     PRIMARY KEY(farmerID)
+     );
+
